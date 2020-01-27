c1f029a7c67c4a505821cf841a060b71a188524c
MarcusJones <jones.0bj3@gmail.com>
2020-01-27 21:18:44 +0100

    update

--- l36-access-levels/src/main.c
+++ l36-access-levels/src/main.c
@@ -26,13 +26,32 @@ void generate_interrupt(void)
 	NVIC_SetPendingIRQ(WWDG_IRQn);
 }
 
+void call_application_task(void)
+{
+	// This is the user
+	uint32_t value = __get_CONTROL();
+	value &= ~0x01;
+	__set_CONTROL(value);
+
+}
+
+void RTOS_init(void)
+{
+	// Does RTOS low level initialization
+
+	// Before running the task, change privilege level!
+	uint32_t value = __get_CONTROL();
+	value |= 0x01;
+	__set_CONTROL(value);
+
+	call_application_task();
+}
+
 int main(void)
 {
 	// This line will simulate an interrupt
 	generate_interrupt();
-
+	RTOS_init();
 	while(1);
 	return 0;
 }
-
-
